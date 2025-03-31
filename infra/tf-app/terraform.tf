856a208fec0cb57b563e8cc9d8b5d7f1cec18ec7
NikkiShaker <shak0039@algonquinlive.com>
2025-03-31 18:19:50 -0400

    Add required_version and empty tf files to satisfy TFLint

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "~> 1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -13,9 +15,4 @@ terraform {
     key                  = "prod.app.tfstate"
     use_oidc             = true
   }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
 }

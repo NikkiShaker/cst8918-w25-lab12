5191308277fac3d4b7e82ce513767e307e22f219
Donna-Smee <dailydosesofdonuts@gmail.com>
2025-03-31 23:45:29 -0400

    Add virtual network and subnet

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -3,3 +3,27 @@
   location = "Canada Central"
 }
 */
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ha000070-vnet"
+  location            = "Canada Central"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "ha000070-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  service_endpoints = ["Microsoft.Storage", "Microsoft.Sql"]
+
+  tags = {
+    environment = "test"
+  }
+}

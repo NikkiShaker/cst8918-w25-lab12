c065353a609bc607adc851bd26950f44590708b2
Donna-Smee <dailydosesofdonuts@gmail.com>
2025-03-31 23:49:03 -0400

    fix: resource name change

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "app_rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "ha000070-vnet"
   location            = "Canada Central"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.app_rg.name
   address_space       = ["10.0.0.0/16"]
 
   tags = {
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = "ha000070-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.app_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
